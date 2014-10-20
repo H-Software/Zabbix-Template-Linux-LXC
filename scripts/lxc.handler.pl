ab5ba1561ecdaca9ec9fbb967fc65acb54efcecf
Patrik Majer <p.majer@tcpisek.cz>
2014-10-20 18:54:57 +0200

    add items: CPU acct

--- scripts/lxc.handler.pl
+++ scripts/lxc.handler.pl
@@ -10,54 +10,56 @@
 my $mode = $ARGV[0];
 my $name = $ARGV[1];
 
-if($mode eq "PID"){
-
-     if( $name =~ /^([a-z]|[A-Z]|[0-9]|_|-|\.)+$/ ){
-        $result = `lxc-info -n $name 2>&1`;
-        if ($result =~ m/pid:([[:space:]]*)((-|[0-9])+)/){
-            print "$2\n";
-            exit 0;
-        }
-        else{
-            print STDERR "ERROR! Wront results (PID)\n";
-            print STDERR "DEBUG: ".$result."\n";
-            exit 33;
-        }
-     }
-     else{
-        print STDERR "ERROR! Wrong input \"name\" (PID)\n";
+#
+#check input variable
+#
+unless( $name =~ /^([a-z]|[A-Z]|[0-9]|_|-|\.)+$/ ){
+        print STDERR "ERROR! Wrong input \"name\"\n";
         exit 2;
+}
+
+#
+#internal rutines
+#
+sub print_error {
+    foreach $msq (@_){
+        print STDERR $msq;
     }
 
+    print STDERR " (". $mode .")\n";
+    exit 2;
+}
+
+sub print_result {
+    foreach $msq (@_){
+        print $msq;
+    }
+
+    print "\n";
+    exit 0;
+}
+
+#
+# main loop
+#
+if($mode eq "PID"){
+        (`lxc-info -n $name 2>&1` =~ m/pid:([[:space:]]*)((-|[0-9])+)/) ? print_result("$2") : print_error("ERROR! Wront results");
 }
 elsif($mode eq "STATE"){
-
-     if( $name =~ /^([a-z]|[A-Z]|[0-9]|_|-|\.)+$/ ){
-        $result = `lxc-info -n $name 2>&1`;
-        if ($result =~ m/state:([[:space:]]*)([A-Z]+)/){
-            print "$2\n";
-            exit 0;
-        }
-        else{
-            print STDERR "ERROR! Wront results (STATE)\n";
-            print STDERR "DEBUG: ".$result."\n";
-            exit 33;
-        }
-     }
-     else{
-        print STDERR "ERROR! Wrong input \"name\" (STATE)\n";
-        exit 2;
-    }
-
+        (`lxc-info -n $name 2>&1` =~ m/state:([[:space:]]*)([A-Z]+)/) ? print_result("$2") : print_error("ERROR! Wront results ");
+}
+elsif($mode eq "CPU-sys"){
+        (`cat /sys/fs/cgroup/lxc/$name/cpuacct.stat 2>&1` =~ m/system([[:space:]]*)([0-9]+)/) ? print_result("$2") : print_result("0");
+}
+elsif($mode eq "CPU-user"){
+        (`cat /sys/fs/cgroup/lxc/$name/cpuacct.stat 2>&1` =~ m/user([[:space:]]*)([0-9]+)/) ? print_result("$2") : print_result("0");
 }
 elsif($mode eq "NEXT"){
 
 }
 else{
-
  print STDERR "ERROR! Unknown mode\n";
-
  exit 1;
-
 }
+
 
