e610f73566cf2d3da924aa393444ac583732076a
Patrik Majer <patrik.majer@atlas.cz>
2016-05-29 21:48:15 +0200

    add centos support for discovery script

--- scripts/lxc.discovery.pl
+++ scripts/lxc.discovery.pl
@@ -1,17 +1,38 @@
 #!/usr/bin/perl
 
+#use strict;
+
 # Patrik Majer (patrik.majer.pisek@gmail.com)
 #
 # Zabbix 2 - LXC containers autodiscovery for linux
 #
-# all parameters listed in /sys/fs/cgroup/lxc/ are returned
+# all containers listed in cgroup/lxc are returned
 #
+
+@result = `ls /sys/fs/cgroup/lxc > /dev/null 2>&1`;
+
+if ( $? == -1 ){
+  #print "command failed: $!\n";
+}
+else {
+  # printf "command exited with value %d", $? >> 8;
+
+  if ( $? == 0 ){
+    #debian
+    our $command="find /sys/fs/cgroup/lxc/* -type d -exec ls -d {} \\;";
+  }
+  else {
+    # centos or another OS
+    our $command="find /cgroup/memory/lxc/* -type d -exec ls -d {} \\;";
+  }
+}
 
 $first = 1;
 print "{\n";
 print "\t\"data\":[\n\n";
 
-for (`find /sys/fs/cgroup/lxc/* -type d -exec ls -d {} \\;`)
+for (`$command 2>/dev/null`)
+#for (`$command`)
 {
 
  my @f = split('/', $_);
@@ -34,5 +55,4 @@ for (`find /sys/fs/cgroup/lxc/* -type d -exec ls -d {} \\;`)
 
 print "\n\t]\n";
 print "}\n";
-
 
